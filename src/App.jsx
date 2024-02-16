4b129c9ad9f443820d4e636d00f2963638cbd91a
Japhetaline <Japhetdamassoh@gmail.com>
2024-02-16 19:05:10 +0100

    sidebar_navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './homePage/homePage'
+import HomePage from './homePage/homePage';
 import AdminPage from './adminPortal/admin'
 import StudentPage from './studentPortal/studentPortal'
 import TeacherPage from './teacherPortal/teacherPortal'
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,67 +1,57 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { MdClose } from 'react-icons/md';
-import { FiMenu } from 'react-icons/fi';
+import React from 'react'
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import { FaHome, FaUser, FaUserGraduate,  FaUserTie,  FaTools  } from "react-icons/fa";
 
-const Navbar = () => {
-  const [navbarOpen, setNavbarOpen] = useState(false);
-  const ref = useRef();
-
-  useEffect(() => {
-    const handler = (event) => {
-      if (navbarOpen && ref.current && !ref.current.contains(event.target)) {
-        setNavbarOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handler);
-
-    return () => {
-      // Cleanup the event listener
-      document.removeEventListener('mousedown', handler);
-    };
-  }, [navbarOpen]);
-
+function Navbar() {
   return (
     <>
-      <nav ref={ref} className="navbar">
-        <button
-          className="toggle"
-          onClick={() => setNavbarOpen((prev) => !prev)}
-        >
-          {navbarOpen ? (
-            <MdClose style={{ width: '32px', height: '32px', color: 'red'}} />
-          ) : (
-            <FiMenu
-              style={{
-                width: '32px',
-                height: '32px',
-                color: 'red'
-              }}
-            />
-          )}
-        </button>
-        <ul className={`menu-nav${navbarOpen ? ' show-menu' : ''}`}>
-          <li>
-            <Link to="/about" onClick={() => setNavbarOpen(false)} >
-           About
-            </Link>
-          </li>
-          <li>
-            <Link to="/faq" onClick={() => setNavbarOpen(false)}>
-              FAQ
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" onClick={() => setNavbarOpen(false)}>
-              Login/Signup
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
-};
+    <a id="nav-expand" href="#">
+  <span class="icon icon-menu"></span>&nbsp;
+  Menu
+</a>
+<nav>
+  <a id="nav-collapse" href="#">
+    <span class="icon icon-cross"></span>
+  </a>
+  
+  <div className="side-menu">
+          <Link to="/">
+            <FaHome color='white'/>
+            <span>Home</span>
+          </Link>
+        </div>
 
-export default Navbar;
+        <div className="side-menu">
+          <Link to="/admin">
+            <FaUser color='white'/>
+            <span>Admin</span>
+          </Link>
+        </div>
+
+        <div className="side-menu">
+          <Link to="/student">
+            <FaUserGraduate color='white' />
+            <span>Student</span>
+          </Link>
+        </div>
+  
+        <div className="side-menu">
+          <Link to="/teacher">
+            <FaUserTie color='white'/>
+            <span>Teacher</span>
+          </Link>
+        </div>
+
+        <div className="side-menu">
+          <Link to="/settings">
+            <FaTools color='white'/>
+            <span>Settings</span>
+          </Link>
+        </div>  
+</nav>
+</>
+  )
+}
+
+export default Navbar
